27da9f3d9c1c311d52aa0fd1a096b088af1ebdd2
Leonardo Delgado Medrano <leonardo.delgado.medrano@gmail.com>
2026-04-07 00:08:39 -0400

    Api keys completo

--- src/routes/tenant.routes.js
+++ src/routes/tenant.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerTenant } = require('../modules/tenants/tenant.controller');
 
-// Ruta final: /api/tenants/register
+// Exponemos la ruta POST /register
 router.post('/register', registerTenant);
 
 module.exports = router;
