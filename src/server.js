8322e0e517ab3e78c6c0fbe60b31b4d1a2c72b7b
Leonardo Delgado Medrano <leonardo.delgado.medrano@gmail.com>
2026-04-19 23:28:12 -0400

    descomentar redis

--- src/server.js
+++ src/server.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 
 const app = require('./app');
 const enableWebhooks = process.env.ENABLE_WEBHOOKS === 'true';
-// const webhookWorker = enableWebhooks ? require('./modules/webhooks/webhook.worker') : null;
-// const webhookPolling = enableWebhooks ? require('./modules/webhooks/webhook.polling') : null;
+const webhookWorker = enableWebhooks ? require('./modules/webhooks/webhook.worker') : null;
+const webhookPolling = enableWebhooks ? require('./modules/webhooks/webhook.polling') : null;
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`🚀 Servidor de FrogPay corriendo en el puerto ${PORT}`);
     if (enableWebhooks) {
-        // webhookWorker.start();
-        // webhookPolling.start();
+        webhookWorker.start();
+        webhookPolling.start();
         console.log('[Webhooks] Desactivados temporalmente (Redis no disponible).');
     } else {
         console.log('[Webhooks] Desactivados. Define ENABLE_WEBHOOKS=true para habilitarlos.');
